8922b1d18bdcc22588c769fa68cb3089fef4b7cc
ravijadar9740 <ravitejjadar5@gmail.com>
2025-12-05 10:54:57 +0530

    Implement number swapping in SwapNumbers.java

---
+++ SwapNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SwapNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping:");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
